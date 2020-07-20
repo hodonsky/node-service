7de77b726a898024b70e33e1fd5ee5f13ad3e1fe
Eric Hodonsky <relic.melex@gmail.com>
2020-07-20 13:41:39 -0700

    Initial Commit

---
+++ dist/Server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _createApplication = _interopRequireDefault(require("./createApplication"));
+
+var _config = _interopRequireDefault(require("./config"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var config = _objectSpread({}, _config.default);
+
+class _default {
+  static configure(updates) {
+    config = _objectSpread(_objectSpread({}, config), updates);
+  }
+
+  constructor(serviceFunctions) {
+    return (0, _createApplication.default)(serviceFunctions, _objectSpread({}, config));
+  }
+
+}
+
+exports.default = _default;
---
+++ dist/config.js
@@ -0,0 +1,33 @@
+"use strict";
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _os = _interopRequireDefault(require("os"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var cpus = _os.default.cpus().length;
+
+var freeMem = _os.default.freemem();
+
+var _default = {
+  cpus,
+  env: process.env.AOB_ENV || "local",
+  freeMem,
+  memory: _os.default.totalmem() / 1024 / 1024 / 1024,
+  mq: {
+    protocol: process.env.MQ_PROTOCOL,
+    hostname: process.env.MQ_HOSTNAME,
+    port: process.env.MQ_PORT,
+    username: process.env.MQ_USERNAME,
+    password: process.env.MQ_PASSWORD,
+    queue: process.env.MQ_QUEUE
+  },
+  port: process.env.PORT,
+  serviceName: process.env.SERVICE_NAME || process.env.HOSTNAME || "Unnamed Service"
+};
+exports.default = _default;
